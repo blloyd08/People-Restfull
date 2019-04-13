d8b48244c52c1710e83dd8d4c1a0cb0389c2c118
Brian Lloyd <blloyd2008@gmail.com>
2019-04-13 12:37:55 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import render_template
+import connexion
+
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
+@app.route('/')
+def home():
+    """
+    This function just reponds to the browser URL
+    localhost:5000/
+
+    :return:    the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
